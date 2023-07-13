ad1716bd4d6b10d0f84d6adf49682c0c57f5c520
McKay-Larsen <mccrakken77@gmail.com>
2023-07-12 17:08:27 -0700

    add simple decorator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello there!!!?');
+function Logger(logString: string) {
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    }
+}
+
+@Logger('LOGGING - PERSON')     // Executes when your class is defined
+class Person {
+    name = 'McKay';
+
+    constructor() {
+        console.log('Constructor creating person object...');
+    }
+}
+
+const pers = new Person();
+console.log(pers);
